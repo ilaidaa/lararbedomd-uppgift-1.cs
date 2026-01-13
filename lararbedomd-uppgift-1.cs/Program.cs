bc63606619f2849cc5d7e8237422b41b6b9a27d1
ilaidaa <ilaida.e@hotmail.com>
2026-01-13 09:48:04 +0100

    Add project files.

---
+++ lararbedomd-uppgift-1.cs/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace lararbedomd_uppgift_1.cs
+{
+    internal class Program
+    {
+
+        // Dessa variabler är static eftersom Main-metoden är static.
+        // En static metod kan bara använda andra static variabler och metoder och sista metoden ska använda variablerna
+        // Därför behöver namn och ålder vara static för att kunna användas i hela programmet.
+        static string name = ""; //tom ska fyllas
+        static int age = 0;
+
+
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
