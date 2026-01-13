24b877eb352df6671936bdda5b103b6a598e5f32
ilaidaa <ilaida.e@hotmail.com>
2026-01-13 09:52:59 +0100

    Skapat första metoden ShowMenu() i en while loop

--- lararbedomd-uppgift-1.cs/Program.cs
+++ lararbedomd-uppgift-1.cs/Program.cs
@@ -12,7 +12,29 @@
 
         static void Main(string[] args)
         {
-           
+            // Variabel som styr om programmet ska fortsätta köra
+            bool running = true;
+
+
+            // Menyn körs i en while-loop tills användaren väljer att avsluta
+            while (running)
+            {
+                ShowMenu();
+
+            }
+        }
+
+
+
+
+
+
+        // Alla metoder
+        static void ShowMenu()
+        {
+            Console.WriteLine("1. Enter your name and age");
+            Console.WriteLine("2. Show when you will turn 100");
+            Console.WriteLine("3. Exit");
         }
     }
 }
