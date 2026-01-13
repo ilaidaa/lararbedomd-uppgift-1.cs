78a230dd08026bee3ff9ceb442c76912f780a20c
ilaidaa <ilaida.e@hotmail.com>
2026-01-13 09:48:48 +0100

    Skapat static variabler som ska användas över hela programmet.

--- lararbedomd-uppgift-1.cs/Program.cs
+++ lararbedomd-uppgift-1.cs/Program.cs
@@ -6,7 +6,7 @@
         // Dessa variabler är static eftersom Main-metoden är static.
         // En static metod kan bara använda andra static variabler och metoder och sista metoden ska använda variablerna
         // Därför behöver namn och ålder vara static för att kunna användas i hela programmet.
-        static string name = ""; //tom ska fyllas
+        static string name = ""; // Tom variabel ska fyllas senare
         static int age = 0;
 
 
